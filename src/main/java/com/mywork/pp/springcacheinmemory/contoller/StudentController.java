a10df19f891d093f070c58b551d968da31715ec3
pramodpatwawork <pramod_patwa2003@yahoo.com>
2022-04-15 20:23:41 +0530

    commited

--- src/main/java/com/mywork/pp/springcacheinmemory/contoller/StudentController.java
+++ src/main/java/com/mywork/pp/springcacheinmemory/contoller/StudentController.java
@@ -1,8 +1,14 @@
 package com.mywork.pp.springcacheinmemory.contoller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mywork.pp.springcacheinmemory.model.Student;
@@ -19,4 +25,10 @@ public class StudentController {
 		System.out.println("Searching by ID  : " + id);
 		return studentService.getStudentByID(id);
 	}
+	
+	@RequestMapping("/foo")
+	  void handleFoo(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	    System.out.println("FOO URL is "+ request.getContextPath());
+		response.sendRedirect(request.getContextPath()+"/students/1");
+	  }
 }
